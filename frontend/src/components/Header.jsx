1f009c0fb77e4bfe6771bf6f47d91e738140d582
Aryan <aryanverma170372@gmail.com>
2022-09-11 00:46:25 +0530

    feat: show logout button for when logged in

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,44 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { logout, reset } from "../features/auth/authSlice";
 
 function Header() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+  };
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">Support Desk</Link>
       </div>
       <ul>
-        <li>
-          <Link to="/login">
-            <FaSignInAlt />
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
+        {user ? (
+          <button className="btn" onClick={onLogout}>
             <FaSignOutAlt />
-            Register
-          </Link>
-        </li>
+            Logout
+          </button>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt />
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaSignOutAlt />
+                Register
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </header>
   );
