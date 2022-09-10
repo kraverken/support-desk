0564f6604ed36a10a724c4679766fdabf711ebdf
Aryan <aryanverma170372@gmail.com>
2022-09-11 01:42:36 +0530

    feat: add cases for login and impolement login

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -33,7 +33,16 @@ export const register = createAsyncThunk(
 
 // Login user
 export const login = createAsyncThunk("auth/login", async (user, thunkAPI) => {
-  console.log(user);
+  try {
+    return await authService.login(user);
+  } catch (error) {
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+
+    return thunkAPI.rejectWithValue(message);
+  }
 });
 
 // Logout user
@@ -72,7 +81,21 @@ export const authSlice = createSlice({
       })
       .addCase(logout.fulfilled, (state) => {
         state.user = null;
-      }); // to remove the login from the state
+      }) // to remove the login from the state
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = true;
+        state.isError = true;
+        state.user = null;
+        state.message = action.payload; // rejectwithvalue will be the payload here
+      });
   },
 });
 
