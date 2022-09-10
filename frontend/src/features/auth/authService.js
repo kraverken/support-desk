1239fee9799f5b61973e44a6f472878738e1d741
Aryan <aryanverma170372@gmail.com>
2022-09-11 01:42:00 +0530

    feat: auth service to login user

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "/api/users";
+const API_URL = "/api/users/";
 // here it will be running on http://localhost:3000/api/users but we want to run on http://localhost:5000 alternatively we can set the proxy in package.json
 
 // Register user
@@ -12,11 +12,21 @@ const register = async (userData) => {
   return response.data;
 };
 
+// login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
 // LogOut user
 const logout = () => localStorage.removeItem("user");
 
 const authService = {
   register,
+  login,
   logout,
 };
 
