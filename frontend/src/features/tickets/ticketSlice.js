4ada28b480463f8f63dfe60342affbf825862fda
Aryan <aryanverma170372@gmail.com>
2022-09-13 03:44:49 +0530

    feat: handling the cases for getTickets

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -70,6 +70,19 @@ export const ticketSlice = createSlice({
         state.isLoading = true;
         state.isError = true;
         state.message = action.payload;
+      })
+      .addCase(getTickets.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getTickets.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.tickets = action.payload;
+      })
+      .addCase(getTickets.rejected, (state, action) => {
+        state.isLoading = true;
+        state.isError = true;
+        state.message = action.payload;
       });
   },
 });
