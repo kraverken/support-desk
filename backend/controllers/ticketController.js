1c69c01cd479f35a8d297945e58e362982a89af1
Aryan <aryanverma170372@gmail.com>
2022-09-12 15:43:12 +0530

    feat: update Ticket functionality

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -101,9 +101,43 @@ const deleteTicket = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true });
 });
 
+// @desc    Update user ticket
+// @route   UPDATE /api/tickets/:id
+// @access  Private
+const updateTicket = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    res.status(404);
+    throw new Error("Ticket not found");
+  }
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
+  const updatedTicket = await Ticket.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true }
+  );
+
+  res.status(200).json(updatedTicket);
+});
+
 module.exports = {
   getTickets,
   getTicket,
   createTicket,
   deleteTicket,
+  updateTicket,
 };
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -5,11 +5,16 @@ const {
   getTicket,
   createTicket,
   deleteTicket,
+  updateTicket,
 } = require("../controllers/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
-router.route("/:id").get(protect, getTicket).delete(protect, deleteTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
