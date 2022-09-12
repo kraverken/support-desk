554a255b4010a7e2b81e9316c0e0ee4b9ad1b7cd
Aryan <aryanverma170372@gmail.com>
2022-09-12 17:33:57 +0530

    feat: auth status initialise

---
+++ frontend/src/hooks/useAuthStatus.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+    setLoading(false);
+  }, [user]);
+
+  return { loggedIn, loading };
+};
+
+// to check if the user is logged in or not and return it in the auth status
