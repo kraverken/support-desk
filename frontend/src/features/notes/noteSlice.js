fe1e367d01958571f4867d9d6abd552a5dbf09dd
Aryan <aryanverma170372@gmail.com>
2022-09-17 19:45:11 +0530

    feat: reducer & service for creating a note

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -31,6 +31,27 @@ export const getNotes = createAsyncThunk(
   }
 );
 
+// create  ticket notes
+export const createNote = createAsyncThunk(
+  "notes/create",
+  async ({ noteText, ticketId }, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      // here we need the bearer token for authorization and using thunkAPI we can access any state so we access the token in user in auth state
+      return await noteService.createNote(noteText, ticketId, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const noteSlice = createSlice({
   name: "note",
   initialState,
@@ -48,6 +69,19 @@ export const noteSlice = createSlice({
         state.notes = action.payload;
       })
       .addCase(getNotes.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      })
+      .addCase(createNote.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createNote.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.notes.push(action.payload); // here with redux we can actually push onto the state rather than reassingning it
+      })
+      .addCase(createNote.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
